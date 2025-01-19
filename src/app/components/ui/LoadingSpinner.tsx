e66a40a05d803123d0a67ac3687e41c6c6e02a43
Jason Olimpio <olimpiojason@gmail.com>
2025-01-19 04:24:12 +0100

    Fix spinner position

--- src/app/components/ui/LoadingSpinner.tsx
+++ src/app/components/ui/LoadingSpinner.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const LoadingSpinner = () => (
   <div className="fixed w-screen h-screen flex justify-center items-center">
     <div
-      className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-solid border-gray-200 border-t-blue-500 rounded-full"
+      className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-solid
+      border-gray-200 border-t-blue-500 rounded-full absolute top-[40%]"
       role="status"
     />
   </div>
